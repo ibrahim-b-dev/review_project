175018b5ef249226b62d32ce47c0a22031058108
Ibrahim, B <ibrahim.dev.b@gmail.com>
2025-08-22 21:30:35 +0300

    uibuild

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const requestLogger = (request, response, next) => {
 app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
+app.use(express.static("dist"))
 
 const generateId = () => {
   const maxId =
